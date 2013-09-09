ce7914dd35e66820248cb82760b50a31bc8a625b
Wouter Bolsterlee <uws@xs4all.nl>
2013-09-09 16:21:55 +0200

    Add setup.py script to install whip-neustar cli script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='whip-neustar',
+    version='0.1',
+    packages=['whip_neustar'],
+    entry_points={
+        'console_scripts': [
+            'whip-neustar = whip_neustar.cli:main',
+        ],
+    }
+)
