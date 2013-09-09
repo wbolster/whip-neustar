c4cdcc981d275135cdba076866ccc5f5e554f6b9
Wouter Bolsterlee <uws@xs4all.nl>
2013-09-09 16:35:42 +0200

    Add CLI module

---
+++ whip_neustar/cli.py
@@ -0,0 +1,38 @@
+"""
+Command line interface module.
+"""
+
+import logging
+import sys
+
+import aaargh
+import ujson
+
+from .reader import iter_records
+
+app = aaargh.App(
+    description="Neustar (formerly Quova) data set utilities.")
+
+
+@app.cmd(description="foobar")
+@app.cmd_arg('filename')
+def convert(filename):
+    out_fp = sys.stdout
+
+    write = out_fp.write
+    dumps = ujson.dumps
+    for doc in iter_records(filename):
+        write(dumps(doc))
+        write('\n')
+
+
+def main():
+    logging.basicConfig(
+        format='%(asctime)s (%(name)s) %(levelname)s: %(message)s',
+        level=logging.INFO,
+    )
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
